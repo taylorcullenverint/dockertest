7cfa13e9a6fdd263ce973955a948436a78499388
Taylor Cullen <taylor.cullen@verint.com>
2018-09-10 15:12:37 +0100

    first commit

---
+++ Sandbox/Controllers/TestController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+
+namespace Sandbox.Controllers
+{
+    [Route("test/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public async Task Test()
+        {
+            var message = new MimeMessage();
+
+            message.From.Add(new MailboxAddress("Sandbox", "sandbox@sandbox.com"));
+            message.To.Add(new MailboxAddress("", "test@fake.com"));
+            message.Subject = "Test Data";
+
+            message.Body = new TextPart("plain")
+            {
+                Text = string.Join(Environment.NewLine, new string[] { "Test", "One", "Two", "Three" })
+            };
+
+            using (var mailClient = new SmtpClient())
+            {
+                await mailClient.ConnectAsync("mail", 1025, SecureSocketOptions.None);
+                await mailClient.SendAsync(message);
+                await mailClient.DisconnectAsync(true);
+            }
+
+        }
+    }
+}
